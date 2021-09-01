00b028daea1104db18933f7dc22b03f567682759
vbiasin <vincent.biasin@outlook.fr>
2021-09-01 19:11:14 +0200

    Travail pour rendre site responsive (CSS)

--- src/main/java/fr/OCP6Escalade/Controllers/OCP6EscladeController.java
+++ src/main/java/fr/OCP6Escalade/Controllers/OCP6EscladeController.java
@@ -6,12 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OCP6EscladeController {
 	
-	@RequestMapping("/")
-	public String startPage() {
-		return "startPage";
-	}
 	
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	public String home() {
 		return "home";
 	}
